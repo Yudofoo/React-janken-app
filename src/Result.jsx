142a0f6220713b262f0a0fcff9107f6aa455592a
Atsuto Tanaka <atsuto@AtsutonoMacBook-Air.local>
2023-10-15 14:40:13 +0900

    layout adjustment

--- src/Result.jsx
+++ src/Result.jsx
@@ -14,12 +14,14 @@ if (result === -1 || result === 2) {
     <>
     {enemySelect ?
     <div className="m-4 border-8 border-red-500">
+    <div className="flex justify-center">
      {enemySelect === 1 && <img src="rdesign_14296.png" alt=""/>}
      {enemySelect === 2 && <img src="rdesign_14297.png" alt=""/>}
      {enemySelect === 3 && <img src="rdesign_14298.png" alt=""/>}
+    </div>
      <div className="flex justify-center text-3xl">Result: {finalResult}</div>
-    </div> : <div></div>
-    }
-    </>
+    </div>: <div></div>
+}
+</>
   )
 }
