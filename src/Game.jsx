7fd74f3a22722e35916a4b224c530a064b1b591e
Atsuto Tanaka <atsuto@AtsutonoMacBook-Air.local>
2023-10-17 21:46:27 +0900

    code format

--- src/Game.jsx
+++ src/Game.jsx
@@ -3,13 +3,13 @@ import { Button } from './Button'
 import { Result } from './Result'
 
 export const Game = () => {
-    const [ userSelect, setUserSelect ] = useState("")
-    const [ enemySelect, setEnemySelect ] = useState("")
-    const result = userSelect - enemySelect
+  const [userSelect, setUserSelect] = useState('')
+  const [enemySelect, setEnemySelect] = useState('')
+  const result = userSelect - enemySelect
   return (
     <div>
-        <Result enemySelect={enemySelect} result={result}/>
-        <Button setUserSelect={setUserSelect} setEnemySelect={setEnemySelect} />
+      <Result enemySelect={enemySelect} result={result} />
+      <Button setUserSelect={setUserSelect} setEnemySelect={setEnemySelect} />
     </div>
-    )
+  )
 }
--- src/Result.jsx
+++ src/Result.jsx
@@ -1,27 +1,33 @@
 export const Result = ({ enemySelect, result }) => {
-  let finalResult = ""
+  let finalResult = ''
 
-if (result === 0) {
+  if (result === 0) {
     finalResult = 'あいこ'
-}
-if (result === 1 || result === -2 ) {
-        finalResult = 'まけ'
-}    
-if (result === -1 || result === 2) {
+  }
+  if (result === 1 || result === -2) {
+    finalResult = 'まけ'
+  }
+  if (result === -1 || result === 2) {
     finalResult = 'かち'
-}
+  }
   return (
     <>
-    {enemySelect ?
-    <div className="m-4 border-8 border-red-500">
-    <div className="flex justify-center">
-     {enemySelect === 1 && <img src="rdesign_14296.png" alt=""/>}
-     {enemySelect === 2 && <img src="rdesign_14297.png" alt=""/>}
-     {enemySelect === 3 && <img src="rdesign_14298.png" alt=""/>}
-    </div>
-     <div className="flex justify-center text-3xl">Result: {finalResult}</div>
-    </div>: <div className="flex justify-center text-5xl text-red-500">Rock-Scissors-Paper Go!</div>
-}
-</>
+      {enemySelect ? (
+        <div className="m-4 border-8 border-red-500">
+          <div className="flex justify-center">
+            {enemySelect === 1 && <img src="rdesign_14296.png" alt="" />}
+            {enemySelect === 2 && <img src="rdesign_14297.png" alt="" />}
+            {enemySelect === 3 && <img src="rdesign_14298.png" alt="" />}
+          </div>
+          <div className="flex justify-center text-3xl">
+            Result: {finalResult}
+          </div>
+        </div>
+      ) : (
+        <div className="flex justify-center text-5xl text-red-500">
+          Rock-Scissors-Paper Go!
+        </div>
+      )}
+    </>
   )
 }
